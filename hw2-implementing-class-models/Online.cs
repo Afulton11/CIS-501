33fbe7a886fee8efc438f35531aefcbb4eef121f
sellersn8463 <32393890+sellersn8463@users.noreply.github.com>
2018-09-13 18:48:31 -0500

    Made changes to Online and OnCampus

--- hw2-implementing-class-models/Online.cs
+++ hw2-implementing-class-models/Online.cs
@@ -1,3 +1,29 @@
 namespace Homework
 {
+    public class Online
+    {
+        private Semester sem;
+        private Course cour;
+        private Faculty teach;
+        private string num;
+        private int c;
+
+        public Online(Semester semester, Course course, Faculty teacher, string number, int cap)
+        {
+            sem = semester;
+            cour = course;
+            teach = teacher;
+            num = number;
+            c = cap;
+        }
+
+        public override string ToString()
+        {
+            return "Semester: " + sem.ToString() +
+                    "\nCourse: " + cour.ToString() +
+                    "\nFaculty: " + teach.ToString() +
+                    "\nNumber: " + num +
+                    "\nCap: " + c;
+        }
+    }
 }

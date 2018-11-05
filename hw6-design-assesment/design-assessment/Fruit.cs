52b45e1f16f4a5bfe310a250ab9796282c1fdf13
Andrew Fulton <afulton20@gmail.com>
2018-11-04 19:14:21 -0600

    Added Fruit class

---
+++ hw6-design-assesment/design-assessment/Fruit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace designassessment
+{
+    //The purpose of the class is to maintain the correct state of Fruit objects.
+
+    public class Fruit
+    {
+        String name;
+        int color;
+        int shape;
+        Genus genus;
+
+        public Fruit(String name, int emp_id, Genus genus)
+        {
+            // Update the data member;
+        }
+
+        int UpdateShape(int salary)
+        {
+            // Update shape of Fruit
+            // ...............
+            // ........
+            return 0;
+        }
+
+        void UpdateGenus(Genus genus)
+        {
+            // Change genus of Fruit
+            // ...code goes here
+            // ......
+        }
+
+        int update_Fruit_database(DatabaseHandler db)
+        {
+            // Update shape of Fruit in database
+            // ...............
+            // ........
+            return 0;
+        }
+    }
+}
